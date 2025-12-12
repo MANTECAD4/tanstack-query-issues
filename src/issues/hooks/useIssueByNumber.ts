e88cb013d13c91093e2d040173b99bac93b684bf
Daniel Martinezzz <daniel.martinez5659@alumnos.udg.mx>
2025-12-12 17:34:26 -0600

    useComments removed - issue and comments request are secuential

--- src/issues/hooks/useIssueByNumber.ts
+++ src/issues/hooks/useIssueByNumber.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getIssueByNumber } from "../actions/get-issue-by-number.action";
+import { getComments } from "../actions/get-comments.action";
 
 export const useIssueByNumber = (issueNumber: number) => {
   const issueQuery = useQuery({
@@ -7,5 +8,11 @@ export const useIssueByNumber = (issueNumber: number) => {
     queryFn: () => getIssueByNumber(issueNumber),
     staleTime: 1000 * 60 * 2,
   });
-  return { issueQuery };
+  const commentsQuery = useQuery({
+    queryKey: [`issues`, issueQuery.data?.number, "comments"],
+    queryFn: () => getComments(issueQuery.data!.number),
+    staleTime: 1000 * 60 * 5,
+    enabled: issueQuery.data !== undefined,
+  });
+  return { issueQuery, commentsQuery };
 };
--- src/issues/views/IssueView.tsx
+++ src/issues/views/IssueView.tsx
@@ -1,15 +1,13 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { IssueComment } from "../components/IssueComment";
 import { FiSkipBack } from "react-icons/fi";
-import { useComments } from "../hooks/useComments";
 import { useIssueByNumber } from "../hooks/useIssueByNumber";
 
 export const IssueView = () => {
   const navigate = useNavigate();
 
   const { id } = useParams();
-  const { issueQuery } = useIssueByNumber(Number(id));
-  const { commentsQuery } = useComments(Number(id));
+  const { issueQuery, commentsQuery } = useIssueByNumber(Number(id));
 
   return (
     <div className="mb-5">
@@ -25,11 +23,20 @@ export const IssueView = () => {
       <h2>
         {issueQuery.data?.title} - {issueQuery.data?.user.login}
       </h2>
+      <div className="flex flex-wrap gap-x-1 gap-y-2">{}</div>
       {/* Primer comentario */}
+      {issueQuery.data && (
+        <IssueComment
+          key={issueQuery.data?.id}
+          body={issueQuery.data?.body}
+          userName={issueQuery.data?.user.login}
+          avatarUrl={issueQuery.data?.user.avatar_url}
+        />
+      )}
       {commentsQuery.isLoading ? (
         <span>Loading...</span>
-      ) : commentsQuery.data && commentsQuery.data.length > 0 ? (
-        commentsQuery.data.map((comment) => (
+      ) : (
+        commentsQuery.data?.map((comment) => (
           <IssueComment
             key={comment.id}
             body={comment.body}
@@ -37,8 +44,6 @@ export const IssueView = () => {
             avatarUrl={comment.user.avatar_url}
           />
         ))
-      ) : (
-        <span>No comments yet</span>
       )}
     </div>
   );
